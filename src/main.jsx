bd89b191a59b7bb554fcc9dd5bba8395c97e8e2e
Rangga Mukti <r4n994mukti@gmail.com>
2025-08-25 08:12:58 +0700

    CONFIG tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,18 @@ import './index.css'
 //import BrowserRouter dari react router
 import { BrowserRouter } from 'react-router';
 
+//import QueryClient dan QueryClientProvider dari react-query
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+//init QueryClient
+const queryClient = new QueryClient()
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
 )
